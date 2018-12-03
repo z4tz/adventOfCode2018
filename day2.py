a781018e2f35f3402892de58af3069b40916d571
z4tz <z4tz87@gmail.com>
2018-12-03 02:27:01 +0100

    adding day 2 and a templateday

---
+++ day2.py
@@ -0,0 +1,36 @@
+from inputreader import aocinput
+from collections import defaultdict
+
+
+def checksum(IDs):
+    twos = 0
+    threes = 0
+    for ID in IDs:
+        chardict = defaultdict(int)
+        for char in ID:
+            chardict[char] += 1
+        if 2 in chardict.values():
+            twos += 1
+        if 3 in chardict.values():
+            threes += 1
+    return twos * threes
+
+
+def duplicate(IDs):
+    for i in range(len(IDs[0])-1):  # -1 to not run over endline char as input is not stripped
+        reduced = list(map(lambda x: x[:i] + x[i+1:], IDs))  # remove char at position i
+        unique = set(reduced)
+        if not len(unique) == len(reduced):
+            for string in unique:
+                reduced.remove(string)
+            return reduced[0]
+
+
+def main(day):
+    data = aocinput(day)
+    print(checksum(data))
+    print(duplicate(data))
+
+
+if __name__ == '__main__':
+    main(2)
--- timing.py
+++ timing.py
@@ -1,7 +1,7 @@
 import timeit
 import os
 
-days = range(1, len(os.listdir('inputs/')) + 1)
+days = range(2, len(os.listdir('inputs/')) + 1)
 runs = 1
 
 
@@ -11,8 +11,7 @@ from day{0} import main
 day = {0}""".format(day)
 
 
-if __name__ == '__main__':
-    for day in days:
-        print("-----## Assignment day {0} ##-----".format(day))
-print("Time used for assignment {0}: {1}s\n\n".format(day, timeit.timeit("main(day)", setup=setupstring(day),
-                                                                         number=runs) / runs))
+for day in days:
+    print("-----## Assignment day {0} ##-----".format(day))
+    print("Time used for assignment {0}: {1}s\n\n".format(day, timeit.timeit("main(day)", setup=setupstring(day),
+                                                                             number=runs) / runs))
